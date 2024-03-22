7940d7a7cf6c0468545eae1b80c495010a146d91
shalant <doug.rosenberg@gmail.com>
2024-03-22 13:17:47 -0500

    struggling to apply migration

---
+++ Basic/Basic/Basic/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Basic.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basic.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    { }
+
+    public DbSet<Game> Games { get; set; }
+}
---
+++ Basic/Basic/Basic/Entity/Game.cs
@@ -0,0 +1,7 @@
+﻿namespace Basic.Entity;
+
+public class Game
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}
